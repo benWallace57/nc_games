1ef4476ea73f661c023af7ffff315f47ecf06dbe
benWallace57 <ben.w142@gmail.com>
2021-11-01 15:54:29 +0000

    added get endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -245,5 +245,57 @@ describe("/reviews", () => {
       const err = await request(app).delete("/api/comments/1").expect(400);
       expect(err.body.msg).toEqual("Bad Request: Invalid Comment ID");
     });
+    test("/api/comments/:comment_id INVALID ID", async () => {
+      const err = await request(app).delete("/api/comments/fish").expect(400);
+
+      expect(err.body.msg).toEqual("Bad Request: invalid input");
+    });
+  });
+});
+
+describe("get endpoints", () => {
+  test("/api should return enpoints.json", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual({
+          "GET /api": {
+            description:
+              "serves up a json representation of all the available endpoints of the api",
+          },
+          "GET /api/categories": {
+            description: "serves an array of all categories",
+            queries: [],
+            exampleResponse: {
+              categories: [
+                {
+                  description:
+                    "Players attempt to uncover each other's hidden role",
+                  slug: "Social deduction",
+                },
+              ],
+            },
+          },
+          "GET /api/reviews": {
+            description: "serves an array of all reviews",
+            queries: ["category", "sort_by", "order"],
+            exampleResponse: {
+              reviews: [
+                {
+                  title: "One Night Ultimate Werewolf",
+                  designer: "Akihisa Okui",
+                  owner: "happyamy2016",
+                  review_img_url:
+                    "https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                  category: "hidden-roles",
+                  created_at: 1610964101251,
+                  votes: 5,
+                },
+              ],
+            },
+          },
+        });
+      });
   });
 });
--- routes/api.router.js
+++ routes/api.router.js
@@ -2,9 +2,22 @@ const apiRouter = require("express").Router();
 const categoriesRouter = require("./categories.router");
 const reviewsRouter = require("./reviews.router");
 const commentsRouter = require("./comments.router");
+const fs = require("fs/promises");
+
+const getEndpoints = async (req, res, next) => {
+  try {
+    const endpoints = await fs.readFile("./endpoints.json");
+
+    res.status(200).send(JSON.parse(endpoints));
+  } catch (err) {
+    next(err);
+  }
+};
 
 apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/comments", commentsRouter);
 apiRouter.use("/categories", categoriesRouter);
+
+apiRouter.get("/", getEndpoints);
 
 module.exports = apiRouter;
